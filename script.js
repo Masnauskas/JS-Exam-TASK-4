967efd2ee64fc60b439686671bf3717f716fa4a8
Tadas <tadasmasnauskas@gmail.com>
2022-10-26 20:25:54 +0300

    added js script get and draw cars

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti 
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
+
+async function getCarData() {
+  try {
+    const response = await fetch("./cars.json");
+    carData = await response.json();
+    console.log(carData);
+    drawCarCards(carData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function drawCarCards(data) {
+  const output = document.getElementById("output");
+
+  data.forEach((itemData) => {
+    const carBrand = document.createElement("h2");
+    carBrand.textContent = itemData.brand;
+    carBrand.classList.add("car-brand");
+
+    const carModel = document.createElement("div");
+    carModel.textContent = itemData.models;
+    carModel.classList.add("car-model");
+
+    // carBrand.append();
+    output.append(carBrand, carModel);
+  });
+}
+
+getCarData();
